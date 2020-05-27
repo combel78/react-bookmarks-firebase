1d00555ef1ef1b40d831f9f27b0aff75823eba4f
dirkmittmann <dirkmittmann@gmail.com>
2020-05-27 13:49:16 +0200

    01_ReactRouter, Login- und Registrierungsseite

--- src/Bookmarks/BookmarkApp.tsx
+++ src/Bookmarks/BookmarkApp.tsx
@@ -1,68 +1,19 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import BookmarksIcon from '@material-ui/icons/Bookmarks';
-import { CssBaseline, AppBar, Toolbar, Container } from '@material-ui/core';
-import BookmarkList from './BookmarkList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import BookmarkHome from './BookmarkHome';
+import Login from './Login';
+import Register from './Register';
 
-const useStyles = makeStyles(theme => ({
-  appBarColor: {
-    backgroundColor: '#625750',
-  },
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  mainContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
-function App() {
-
-  const classes = useStyles();
-
-  return (
-    <React.Fragment>
-      <CssBaseline />
-
-      <AppBar className={classes.appBarColor} position="relative">
-        <Toolbar>
-          <BookmarksIcon className={classes.icon} />
-          <Typography variant="h6" color="inherit" noWrap>
-            React Bookmarks
-          </Typography>
-        </Toolbar>
-      </AppBar>
-
-      <main>
-        <div className={classes.mainContent}>
-          <Container maxWidth="lg">
-            <Typography component="h3" variant="h4" align="center" color="textPrimary" gutterBottom>
-              React Bookmarks
-            </Typography>
-            <BookmarkList />
-          </Container>
-        </div>
-      </main>
-
-      <footer className={classes.footer}>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Dirk Mittmann 2020
-        </Typography>
-      </footer>
-
-    </React.Fragment>
-  );
+const BookmarkApp: React.FC = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={BookmarkHome} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+            </Switch>
+        </Router>
+    );
 }
 
-export default App;
+export default BookmarkApp;
