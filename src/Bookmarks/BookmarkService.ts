e1320bfc3dd9776099923cd75e105c33ed862c5d
dirkmittmann <dirkmittmann@gmail.com>
2020-06-03 21:13:01 +0200

    03_Firebase-CloudStore-Service-Anbindung

--- src/Bookmarks/BookmarkService.ts
+++ src/Bookmarks/BookmarkService.ts
@@ -1,15 +1,20 @@
+import FirebaseService from "./FirebaseService";
+
 const initialBookmarks: Array<BookmarkType> = [
     {
+        id: 1,
         name: "ReactJs",
         url: "http://www.reactjs.org",
         description: "React Start Page"
     },
     {
+        id: 2,
         name: "Create React App",
         url: "https://create-react-app.dev",
         description: "create-react-app ist eine Starthilfe zur Erstellung von React-Projekten"
     },
     {
+        id: 3,
         name: "A Guide to Flexbox",
         url: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
         description: "Flexbox CSS Tutorial"
@@ -18,74 +23,88 @@ const initialBookmarks: Array<BookmarkType> = [
 
 class BookmarkService {
 
+    /* Firestore-Objekt */
+    private db: firebase.firestore.Firestore = FirebaseService.getDb();
+
     /* liefere alle Bookmarks */
-    getBookmarks() {
-        return this.getBookmarksSort(0);
+    public onBookmarks = (setBookmarksCallback: any) => {
+        const docRef = this.db.collection('bookmarks').doc('bookmarkData');
+        docRef.get().then((document) => {
+            if(document.exists) {
+                docRef.onSnapshot((doc: any) => {
+                    setBookmarksCallback(JSON.parse(doc.data()['bookmarks']));
+                })                
+            } else {
+                this.saveBookmarksFirestore(JSON.stringify(initialBookmarks));
+                setBookmarksCallback(initialBookmarks);
+            }
+        });
     }
 
     /* liefere alle Bookmarks mit Sortierung */
-    getBookmarksSort(sortType: number) {
-        //wenn der localStorage leer ist, befülle diesen mit den drei initialen Datensätzen
-        if(!localStorage.getItem('bookmarks')) {
-            initialBookmarks.map((b) => this.addBookmark(b));
-        }
-        const bookmarks: Array<BookmarkType> = JSON.parse(localStorage.getItem('bookmarks') || '');
-        if(sortType === 0) {
+    public bookmarksSort(sortType: number, bookmarks: Array<BookmarkType>, setBookmarksCallback: any) {
+        if (sortType === 0) {
             //nach id
             bookmarks.sort((a: BookmarkType, b: BookmarkType) => (a.id || 0) < (b.id || 0) ? -1 : (a.id || 0) > (b.id || 0) ? 1 : 0);
         }
-        if(sortType === 1) {
+        if (sortType === 1) {
             //nach id absteigend
             bookmarks.sort((a: BookmarkType, b: BookmarkType) => (a.id || 0) > (b.id || 0) ? -1 : (a.id || 0) < (b.id || 0) ? 1 : 0);
         }
-        if(sortType === 2) {
+        if (sortType === 2) {
             //nach name
             bookmarks.sort((a: BookmarkType, b: BookmarkType) => (a.name || '') < (b.name || '') ? -1 : (a.name || '') > (b.name || '') ? 1 : 0);
         }
-        if(sortType === 3) {
+        if (sortType === 3) {
             //nach name absteigend
             bookmarks.sort((a: BookmarkType, b: BookmarkType) => (a.name || '') > (b.name || '') ? -1 : (a.name || '') > (b.name || '') ? 1 : 0);
         }
-        return bookmarks;
+        setBookmarksCallback(bookmarks);
+    }
+
+    /* speichere ein Bookmarks-Array */
+    private saveBookmarksFirestore(bookmarks: string) {
+        const docRef = this.db.collection('bookmarks').doc('bookmarkData');
+        docRef.set({
+            bookmarks: bookmarks
+        });
     }
 
     /* füge eine Bookmark hinzu */
-    addBookmark(bookmark: BookmarkType) {
-        if(bookmark !== undefined) {
-            const bookmarks: Array<BookmarkType> = JSON.parse(localStorage.getItem('bookmarks') || '[]');
+    public addBookmark = (bookmarks: Array<BookmarkType>, bookmark: BookmarkType) => {
+        if (bookmark !== undefined) {
             let maxId: number = 0;
             bookmarks.map((b) => maxId = Math.max(maxId, (b.id || 0)));
             bookmark.id = (maxId + 1);
             bookmarks.push(bookmark);
-            localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+            this.saveBookmarksFirestore(JSON.stringify(bookmarks));
         }
     }
 
     /* aktualisiere eine Bookmark anhand ihrer id */
-    updateBookmark(bookmark: BookmarkType) {
-        if(bookmark && bookmark.id) {
-            const bookmarks: Array<BookmarkType> = JSON.parse(localStorage.getItem('bookmarks') || '[]');
+    public updateBookmark = (bookmarks: Array<BookmarkType>, bookmark: BookmarkType) => {
+        if (bookmark && bookmark.id) {
             bookmarks.map((b, i) => {
-                if(b.id === bookmark.id) {
+                if (b.id === bookmark.id) {
                     bookmarks.splice(i, 1, bookmark);
                 }
                 return true;
             })
-            localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+            this.saveBookmarksFirestore(JSON.stringify(bookmarks));
         }
     }
 
     /* lösche eine Bookmark */
-    deleteBookmark(bookmark: BookmarkType) {
-        if(bookmark && bookmark.id) {
-            const bookmarks: Array<BookmarkType> = JSON.parse(localStorage.getItem('bookmarks') || '[]');
+    public deleteBookmark = (bookmarks: Array<BookmarkType>, bookmark: BookmarkType, setBookmarksCallback: any) => {
+        if (bookmark && bookmark.id) {
             bookmarks.map((b, i) => {
-                if(b.id === bookmark.id) {
+                if (b.id === bookmark.id) {
                     bookmarks.splice(i, 1);
                 }
                 return true;
             })
-            localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+            this.saveBookmarksFirestore(JSON.stringify(bookmarks));
+            setBookmarksCallback(bookmarks);
         }
     }
 }
